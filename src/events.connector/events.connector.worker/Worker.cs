658f6818c3fc9644da99fd42dc6a027ea6217caf
Bruno Fillion <brunofillion@Mac-Pro-de-Bruno.local>
2020-01-28 17:01:20 -0500

    Archive initiale v2

--- src/events.connector/events.connector.worker/Worker.cs
+++ src/events.connector/events.connector.worker/Worker.cs
@@ -1,10 +1,30 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
 namespace events.connector.worker
 {
-    public class Worker
+    public class Worker : BackgroundService
     {
-        public Worker()
+        private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
+
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
         }
     }
 }
